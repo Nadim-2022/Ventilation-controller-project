916613ddf230ae425474e1a11a51b05a18d0312e
Nadim-2022 <i.am.nadim95@gmail.com>
2024-03-01 18:24:37 +0200

    Display, encoder classes Updated

--- interupt_handler.h
+++ interupt_handler.h
@@ -8,33 +8,20 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
+#include <iostream>
 
-
+// Gpio 12 for rotary Button
 class InterruptHandler {
 private:
-    static uint8_t pinA;
-    static uint8_t pinB;
-    static uint8_t pinC;
-    static int8_t lastEncoded;
-    static int8_t encoderValue;
+    static void callback(uint gpio, uint32_t events);
+    static uint16_t timestamp;
+    static int count;
+
 
 public:
-    InterruptHandler(uint8_t PinA0, uint8_t pinB0, uint8_t pinC0)
-    {
-        pinA = PinA0;
-        pinB = pinB0;
-        pinC = pinC0;
-        lastEncoded = 0;
-        encoderValue = 0;
-    }
-    void handle_interrupts();
-   void setupPin(uint8_t pin) {
-        gpio_init(pin);
-        gpio_set_dir(pin, GPIO_IN);
-        gpio_pull_up(pin);
-        gpio_set_irq_enabled_with_callback(pin, GPIO_IRQ_EDGE_RISE, true, &handle_interrupts);
 
-    }
+    InterruptHandler(uint8_t PinA0, uint8_t pinC0);
+    void setupPin(uint8_t pin);
 };
 
 #endif //VENTILATION_CONTROLLER_PROJECT_INTERUPT_HANDLER_H
